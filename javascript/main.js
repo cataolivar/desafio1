fde04e84bab3188cebdc335173c629109e3f9277
Cata Olivar <cataolivar@gmail.com>
2022-04-20 10:44:34 -0300

    0.10

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,7 @@ const resta = (a,b) => a-b;
 const multiplicar = (a,b) => a*b;
 
 let precio = prompt ("Ingrese su monto para aplicar el descuento del 10%.")
-let descuento = 10;
+let descuento = precio*0.10;
 let nuevoPrecio = resta (precio, descuento);
 
 alert ("El nuevo precio con el descuento aplicado es de $" + nuevoPrecio);
