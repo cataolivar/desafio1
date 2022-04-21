7e4ea2530168d03809c03b28a581859ef09f8bfe
Cata Olivar <cataolivar@gmail.com>
2022-04-21 18:08:30 -0300

    array

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,20 @@
+/*
+ARRAY + PUSH (opcion 1)
+let carrito = []
+
+let producto = prompt ("Ingrese productos a su carrito. Para finalizar escriba listo")
+
+while (producto != "listo")
+{
+    carrito.push (producto);
+    alert (carrito)
+    producto = prompt ("Ingrese otro producto. Para finalizar escriba listo")
+}
+alert ("Este es su carrito de compras: ")
+alert (carrito)
+*/
+
+//SIMULADOR
 const resta = (a,b) => a-b;
 const multiplicar = (a,b) => a*b;
 
@@ -11,5 +28,12 @@ let cantidad = prompt ("Ahora ingrese la cantidad de articulos")
 let nuevoPrecio1 = multiplicar (nuevoPrecio,cantidad);
 
 alert ("El total es de $" + nuevoPrecio1)
+
+// ARRAY + JOIN (opcion 2)
+
+let array = ["Televisor", "Celular", "Laptop", ""];
+
+alert ("Gracias a su descuento, usted puede elegir entre uno de estos productos y recibir otro descuento : " + array.join (" + 15% de desc. "));
+prompt ("Indique cual producto desea.");
 
 
